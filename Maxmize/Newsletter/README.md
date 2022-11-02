5a6baf710c7ec251ec380c137c5b553e4dd59137
Mostafa Elkholy <mostafa-elkholy@users.noreply.github.com>
2022-11-02 23:36:04 +0200

    Add files via upload

---
+++ Maxmize/Newsletter/README.md
@@ -0,0 +1,4 @@
+# How to include coupon code in newsletter subscription success email in Magento 2
+# See the video about this tutorial
+## Youtube: https://www.youtube.com/watch?v=sYa5xilq2Rg&index=40&list=PL98CDCbI3TNvPczWSOnpaMoyxVISLVzYQ
+## Facebook: https://www.facebook.com/giaphugroupcom/videos/1962801800692028/
---
+++ Maxmize/Newsletter/registration.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * MaximizeStudio, Mostafa Elkholy Co., Ltd.
+ *
+ * NOTICE OF LICENSE
+ *
+
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Maxmize
+ * @package     Maxmize_Newsletter
+ * @copyright   Copyright (c) 2003-2019 MaxmizeStudio Co., Ltd. All rights reserved. (https://maxmizestudio.com/), Mostafa Elkholy
+ * @license     https://maxmizestudio.com/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Maxmize_Newsletter',
+    __DIR__
+);
